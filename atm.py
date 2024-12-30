0ff230d6edc6154464d599919063c31c0cbdb364
engineerjkk <engineerjkk@naver.com>
2024-12-31 01:22:09 +0900

    initial_pipeline

---
+++ atm.py
@@ -0,0 +1,6 @@
+class ATMController:
+    def __init__(self):
+        pass
+
+    def insert_card(self, card):
+        pass
---
+++ test_atm.py
@@ -0,0 +1,11 @@
+import unittest
+
+class TestATMController(unittest.TestCase):
+    def setUp(self):
+        self.atm = ATMController()
+
+    def test_insert_card(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
