bd39b930f9fa6e827ea13eff8fbbb533a9f9c55a
engineerjkk <engineerjkk@naver.com>
2024-12-31 01:27:05 +0900

    add card class

--- atm.py
+++ atm.py
@@ -1,6 +1,17 @@
+class Card:
+    def __init__(self, card_number: str, pin: str):
+        self.card_number = card_number
+        self.pin = pin
+
+    def validate_pin(self, input_pin: str) -> bool:
+        return self.pin == input_pin
+
 class ATMController:
     def __init__(self):
-        pass
+        self.inserted_card = None
 
-    def insert_card(self, card):
-        pass
+    def insert_card(self, card: Card) -> bool:
+        if card:
+            self.inserted_card = card
+            return True
+        return False
--- test_atm.py
+++ test_atm.py
@@ -3,9 +3,8 @@ import unittest
 class TestATMController(unittest.TestCase):
     def setUp(self):
         self.atm = ATMController()
+        self.card = Card("123456", "1234")
 
     def test_insert_card(self):
-        pass
-
-if __name__ == '__main__':
-    unittest.main()
+        self.assertTrue(self.atm.insert_card(self.card))
+        self.assertFalse(self.atm.insert_card(None))
